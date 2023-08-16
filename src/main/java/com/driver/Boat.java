4dedeaecf9933067c0b0cc8ce5dd4b03707c64df
Nilesh Bahetwar <bahetwarnilesh2310@gmail.com>
2023-08-16 17:29:48 +0530

    implement all interface fun

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,11 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
+    public Boat(String name,int capacity){
+        this.name=name;
+        this.capacity=capacity;
+    }
+
     public String getName() {
         return name;
     }
@@ -20,10 +25,11 @@ public class Boat implements WaterVehicle{
         this.capacity = capacity;
     }
 
+    @Override
     public String getVehicleName(){
         return this.name;
     }
-
+   @Override
     public int getVehicleCapacity(){
         return this.capacity;
     }
